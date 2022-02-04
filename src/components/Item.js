8de9c7174c978646efa143d0857ac2e25d312daa
Azonkeu <mazonkeu@gmail.com>
2022-02-04 14:20:35 +0100

    Create the Add book action

---
+++ src/components/Item.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
+
+const Item = (props) => {
+  const {
+    book: { id, title, author },
+  } = props;
+  const dispatch = useDispatch();
+  const removeBookFromStore = () => {
+    dispatch(removeBook({ id }));
+  };
+
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      margin: '1% auto 0 auto',
+      padding: '2%',
+      backgroundColor: 'white',
+      width: '90%',
+    }}
+    >
+      <div style={{ display: 'flex', flexDirection: 'column', width: '50%' }}>
+        <p>Action</p>
+        <h2>{title}</h2>
+        <p style={{ color: 'blue' }}>{author}</p>
+        <div style={{ display: 'flex', flexDirection: 'row', color: 'blue' }}>
+          <button type="button" style={{ marginRight: '2%' }}>Comments</button>
+          <button type="button" style={{ marginRight: '2%' }} onClick={removeBookFromStore}>Remove</button>
+          <button type="button" style={{ marginRight: '2%' }}>Edit</button>
+        </div>
+      </div>
+      <div />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '20%',
+      }}
+      >
+        <button
+          type="button"
+          style={{
+            padding: '3%',
+            backgroundColor: 'blue',
+            color: 'white',
+            width: '100%',
+            border: 'none',
+          }}
+        >
+          Update progress
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Item.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    author: PropTypes.string,
+  }).isRequired,
+};
+
+export default Item;
